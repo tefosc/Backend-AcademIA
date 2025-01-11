b0bc8cea1532ee6d2e5223298ae83279e579ffc9
Alejandro Stefano Solis Martos <asolism17_1@unc.edu.pe>
2025-01-11 10:38:01 -0500

    add .env

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ const express = require("express");
 const mongoose = require("mongoose");
 const bodyParser = require("body-parser");
 const cors = require("cors");
+require("dotenv").config(); // Carga las variables de entorno desde .env
 
 // Configuración del servidor
 const app = express();
@@ -10,10 +11,11 @@ app.use(cors());
 
 // Conexión a MongoDB
 mongoose
-  .connect(
-    "mongodb+srv://asolism171:tAwzfkU9Cwv7Uwt6@cluster-academia.6ugpk.mongodb.net/",
-    { useNewUrlParser: true, useUnifiedTopology: true }
-  )
+  .connect(process.env.MONGO_URI, {
+    // Usar solo la variable de entorno
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
   .then(() => console.log("Conectado a MongoDB"))
   .catch((err) => console.error("Error al conectar a MongoDB:", err));
 
@@ -60,7 +62,7 @@ app.post("/questions", async (req, res) => {
 });
 
 // Iniciar el servidor
-const PORT = 3000;
+const PORT = process.env.PORT || 3000; // Usar el puerto dinámico de Glitch
 app.listen(PORT, () => {
   console.log(`Servidor corriendo en http://localhost:${PORT}`);
 });
